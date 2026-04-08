7e5c9402a4e54210f4d27d09c23e98666c34256f
unknown <adammustaqeemhalid@gmail.com>
2026-04-08 14:53:26 +0800

    removing import Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Contact from './components/Contact'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
-import Projects from './components/Projcts'
 import Skills from './components/Skills'
 import WorkExperience from './components/WorkExperience'
 
