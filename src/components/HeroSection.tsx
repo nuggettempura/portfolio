fa9f3a4bea2fdb2dec35499038ad25e1f62f613c
unknown <adammustaqeemhalid@gmail.com>
2026-04-09 01:18:16 +0800

    adding some stuff

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,9 +3,9 @@ import { motion } from "motion/react"
 const HeroSection = () => {
     return (
         <motion.section id="about" className='hero t-10 pb-10 flex flex-col gap-4 p-4' initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.7, ease: "easeOut" }}>
-            <div className="flex justify-start w-max gap-6">
-                <p className="tag w-max text-lg">Full Stack Software Engineer</p>
-                <p className="tag w-max text-lg">Frontend Developer</p>
+            <div className="flex flex-wrap justify-start gap-6">
+                <p className="tag w-max text-md">Full Stack Software Engineer</p>
+                <p className="tag w-max text-md">Frontend Developer</p>
             </div>
             <h1 className="text-5xl">Adam Halid</h1>
             {/* <h2 className='text-2xl'>Software Engineer</h2> */}
