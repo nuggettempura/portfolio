ee967d9b0271e3f32118854707ec0764b8c56c5b
unknown <adammustaqeemhalid@gmail.com>
2026-04-09 01:01:09 +0800

    removed Projects for now

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,27 +1,27 @@
-import { motion, useScroll, useTransform } from "motion/react"
-import { projects } from "../data"
-import type { ProjectData } from "../data"
-import { useRef } from "react"
+// import { motion, useScroll, useTransform } from "motion/react"
+// import { projects } from "../data"
+// import type { ProjectData } from "../data"
+// import { useRef } from "react"
 
-const Projects = () => {
-    const targetRef = useRef(null);
-    const { scrollYProgress } = useScroll({ target: targetRef });
-    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-55"])
-    return (
-        <section id="projects" className='relative' ref={targetRef}>
-            <h2 className="section text-2xl"></h2>
-            {projects.map((project: ProjectData, index: number) => (
-                <motion.div className="h-[500vh]" key={index}>
-                    <div className="h-[100vh] sticky top-0 flex items-center justify-start"></div>
-                    <img src={project.image} alt="Project Image" />
-                    <p className="text-xl">{project.name}</p>
-                    <button className="primary-button">
-                        <a href={project.link} target="_blank"></a>
-                    </button>
-                </motion.div>
-            ))}
-        </section>
-    )
-}
+// const Projects = () => {
+//     const targetRef = useRef(null);
+//     const { scrollYProgress } = useScroll({ target: targetRef });
+//     const x = useTransform(scrollYProgress, [0, 1], ["0%", "-55"])
+//     return (
+//         <section id="projects" className='relative' ref={targetRef}>
+//             <h2 className="section text-2xl"></h2>
+//             {projects.map((project: ProjectData, index: number) => (
+//                 <motion.div className="h-[500vh]" key={index}>
+//                     <div className="h-[100vh] sticky top-0 flex items-center justify-start"></div>
+//                     <img src={project.image} alt="Project Image" />
+//                     <p className="text-xl">{project.name}</p>
+//                     <button className="primary-button">
+//                         <a href={project.link} target="_blank"></a>
+//                     </button>
+//                 </motion.div>
+//             ))}
+//         </section>
+//     )
+// }
 
-export default Projects
+// export default Projects
